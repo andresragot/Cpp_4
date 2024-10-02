31f1a93c85b04d8d578fc921b334095c79feef4c
Andrés Ragot <andresragot99@gmail.com>
2024-10-02 16:52:08 +0200

    Update Main.cpp

--- Main.cpp
+++ Main.cpp
@@ -1,33 +1,37 @@
 #include <iostream>
 using namespace std;
 
-struct Nodo{
+struct Nodo
+{
 	int dato;
 	Nodo *siguiente;
 };
 
-void agregarPila(Nodo *&Pila, int n){
+void agregarPila(Nodo *&Pila, int n)
+{
 	struct Nodo UltimoNodo = Nodo();
 	UltimoNodo.dato = n;
 	//verificamos que este no sea el primer nodo, porque si lo es no podemos apuntar a nada 
-	if(Pila != nullptr){
+	if (Pila != nullptr)
+	{
 		(*Pila).siguiente = &UltimoNodo;
 	}
 	Pila = &UltimoNodo;
 }
 
-void sacarPila(Nodo *&Pila, int &n){
+void sacarPila(Nodo *&Pila, int &n)
+{
 	struct Nodo Aux;
 	n = Aux.dato;
-	if((Pila-1)==nullptr){
+	if ((Pila-1) == nullptr)
+	{
 		Pila--;
 	}
 	
 	delete &Aux;
 }
 
-int main(){
-	
-	
+int main()
+{		
 	return 0;
 }
